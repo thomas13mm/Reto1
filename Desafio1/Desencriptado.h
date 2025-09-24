178d536c7e16f2d568da3c341693436addcbc43f
cristianflorezm <cristian.florezm@udea.edu.co>
2025-09-24 15:31:48 -0500

    Avance en implementacion de desencriptarTexto: bucles de prueba y validacion de caracteres

--- Desafio1/Desencriptado.h
+++ Desafio1/Desencriptado.h
@@ -1,6 +1,9 @@
 #ifndef DESENCRIPTADO_H
 #define DESENCRIPTADO_H
 
+#include <iostream>
+using namespace std;
+
 unsigned char aplicarXOR(unsigned char caracter, unsigned char K) {
     /*Descripcion:
     *  Funcion encargada de aplicar la operacion XOR entre un caracter y la clave K.
@@ -54,22 +57,49 @@ bool esCaracterInvalido(unsigned char caracter) {
     return true;
 }
 
-char* desencriptarTexto(const char* textoEncriptado, unsigned int n, unsigned char* K, unsigned int* N ) {
+void desencriptarTexto(char* textoEncriptado, unsigned int n, unsigned char& K, unsigned int& N, char*& arrCaracteres, char*& arrNumeros) {
     /*Descripcion:
-     *  Funcion encargada de intentar desencriptar un texto encriptado usando
-     *  un par (K, N). Aplica XOR y rotacion a la derecha, y valida que el 
-     *  resultado contenga unicamente caracteres permitidos (A-Z, a-z, 0-9).
-     *
-     *Args:
-     *  -(const char*) textoEncriptado: Texto de entrada (arreglo de caracteres encriptados).
-     *  -(unsigned int) n: Longitud del texto encriptado.
-     *  -(unsigned char) K: Clave para la operacion XOR.
-     *  -(unsigned int) N: Numero de posiciones de rotacion a la derecha.
-     *  -(char*) textoDesencriptado: Arreglo dinamico donde se almacena el resultado.
-     *
-     *return: (bool) true si todos los caracteres desencriptados son validos,
-     *               false si se encuentra al menos un caracter invalido.
-     */
+    *  Funcion encargada de desencriptar un texto encriptado, probando diferentes combinaciones
+    *  de K y N hasta obtener un resultado valido (caracteres alfanumericos).
+    *
+    *Args:
+    *  -(char*) textoEncriptado: Apuntador al arreglo con el texto encriptado.
+    *  -(unsigned int) n: Longitud del texto encriptado.
+    *  -(unsigned char&) K: Referencia a la clave XOR correcta (resultado).
+    *  -(unsigned int&) N: Referencia al numero de rotaciones correcto (resultado).
+    *  -(char*&) arrCaracteres: Apuntador a un arreglo dinamico que contendra
+    *                           los caracteres desencriptados validos.
+    *  -(char*&) arrNumeros: Apuntador a un arreglo dinamico que contendra 
+    *                        unicamente los numeros desencriptados.
+    *
+    *return: (void) Los resultados se devuelven a traves de las referencias
+    *                y apuntadores dinamicos.
+    */
+
+    char* temp = new char[n];
+
+    for (unsigned int kPrueba = 0; kPrueba < 256; kPrueba++) {
+        for (unsigned int nPrueba = 0; nPrueba < 8; nPrueba++) {
+
+            bool valido = true;
+            for (unsigned int i = 0; i < n; i++) {
+                unsigned char c = textoEncriptado[i];
+
+                c = aplicarXOR(c, (unsigned char)kPrueba);
+                c = rotarDerecha(c, nPruebas);
+
+                if (esCaracterInvalido(c)) {
+                    valido = false;
+                    break;
+                }
+
+
+
+            }
+
+
+        }
+    }
 }
 
 #endif // DESENCRIPTADO_H
